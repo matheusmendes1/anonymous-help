d38341c0d921b78c809aee4b8a54b0fa728d5c05
Matheus Mendes <matheusmendes1@hotmail.com>
2020-09-05 10:09:22 -0300

    Criação de Usuários - 05/09/2020 10:09AM

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
+import 'reflect-metadata';
+import cors from 'cors';
+
 import express from 'express';
+
+import routes from './routes';
+
+import './database';
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
 app.listen(3333, () => {
   // eslint-disable-next-line no-console
-  console.log('Server started on port 3333!');
+  console.log('Server running on port 3333!');
 });
